602bcd82b3f92342673c5a3190b56109a78dd4c3
Ian Scott <chesnekov@gmail.com>
2020-07-28 10:44:16 -0700

    updates AnimalController.cs with all routes and calls
    Co-authored-by: SPdowns <spdowns@protonmail.com>

--- AnimalShelter/Controllers/AnimalController.cs
+++ AnimalShelter/Controllers/AnimalController.cs
@@ -1,11 +1,42 @@
 using Microsoft.AspNetCore.Mvc;
 using AnimalShelter.Models;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace AnimalShelter.Controllers
 {
-  public class ObjectPlural_Controller : Controller
+  public class AnimalsControler : Controller
   {
-    
+    private readonly AnimalShelterContext _dB;
+
+    public AnimalsController(AnimalShelterContext dB)
+    {
+      _dB = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Animal> model = _db.Animals.ToList();
+      return View(model);
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Animal animal)
+    {
+      _db.Animals.Add(animal)
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      Animal thisAnimal =_db.Animals.FirstOrDefault(animals => animals.AnimalId == id);
+      return View(thisAnimal);
+    }
   }
 }
